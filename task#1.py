e785bbd91f76642a0e9b89f4f415b3881305904d
AAYUSH ANAND SINHA <98117714+Aayushsinha09@users.noreply.github.com>
2023-09-10 03:46:32 +0530

    Add files via upload

---
+++ task#1.py
@@ -0,0 +1,55 @@
+#1. Add
+#2. Substract
+#3. Multiply
+#4. Divide
+
+print("select an operation to perform: ")
+
+print("#1. Add")
+
+print("#2. Substract")
+
+print("#3. Multiply")
+
+print("#4. Divide")
+
+a = int(input("ENTER YOUR CHOICE : "))
+
+if a == 1:
+    
+    
+    b =int(input("enter a number : "))
+    c =int(input("enter a number : "))
+    print("Sum of two numbers : ",b+c)
+    
+    
+elif a == 2:
+    
+    
+    
+    
+    
+    c = int(input("enter a number : "))
+    d = int(input("enter a number : "))
+    print("substraction of two numbers",c-d)
+    
+    
+elif a == 3:
+    
+    
+     d = int(input("enter a number : "))
+     e = int(input("enter a number : "))
+     print("multiplication of two numbers",d*e)
+     
+     
+     
+elif a == 4:
+    
+    
+    x = int(input("enter a number : "))
+    
+    
+    y = int(input("enter a number : "))
+    
+    
+    print("division of two numbers", x/y)  
